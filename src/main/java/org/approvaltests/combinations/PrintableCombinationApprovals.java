263c68fb1a60753cb91e668b2dcf2312bb90d5ed
Michel Grootjans <michel.grootjans@gmail.com>
2023-04-23 11:25:16 +0200

    move all testmethods

--- src/main/java/org/approvaltests/combinations/PrintableCombinationApprovals.java
+++ src/main/java/org/approvaltests/combinations/PrintableCombinationApprovals.java
@@ -1,8 +1,13 @@
 package org.approvaltests.combinations;
 
+import org.lambda.functions.Function1;
 import org.lambda.functions.Function2;
 
 public class PrintableCombinationApprovals {
+    public <IN1, OUT> void verifyAllCombinations(Function1<IN1, OUT> call, IN1[] parameters1) {
+        CombinationApprovals.verifyAllCombinations(call, parameters1);
+    }
+
     public <IN1, IN2, OUT> void verifyAllCombinations(Function2<IN1, IN2, OUT> call, IN1[] parameters1, IN2[] parameters2) {
         CombinationApprovals.verifyAllCombinations(call, parameters1, parameters2);
     }
--- src/test/java/org/approvaltests/combinations/CombinationTest.java
+++ src/test/java/org/approvaltests/combinations/CombinationTest.java
@@ -22,17 +22,17 @@ public class CombinationTest
   {
     Integer[] points = new Integer[]{4, 5, 10};
     String[] words = new String[]{"Bookkeeper", "applesauce"};
-    CombinationApprovals.verifyAllCombinations((i, s) -> s.substring(0, i), points, words);
+    approvals.verifyAllCombinations((i, s) -> s.substring(0, i), points, words);
   }
   @Test
   public void test1Parameter()
   {
-    CombinationApprovals.verifyAllCombinations(i -> i * i, new Integer[]{1, 2, 3, 4, 5});
+    approvals.verifyAllCombinations(i -> i * i, new Integer[]{1, 2, 3, 4, 5});
   }
   @Test
   public void testPassMethod()
   {
-    CombinationApprovals.verifyAllCombinations(this::processCall, new Integer[]{1, 2, 3, 4, 5},
+    approvals.verifyAllCombinations(this::processCall, new Integer[]{1, 2, 3, 4, 5},
         new String[]{"a", "b", "c", "d"});
   }
   public Object processCall(Integer i, String s)
@@ -49,7 +49,7 @@ public class CombinationTest
     // begin-snippet: CombinationsStartingPoint
     String[] inputs1 = {"input1.value1", "input1.value2"};
     String[] inputs2 = {"input2.value1", "input2.value2", "input2.value3"};
-    CombinationApprovals.verifyAllCombinations((a, b) -> "placeholder", inputs1, inputs2);
+    approvals.verifyAllCombinations((a, b) -> "placeholder", inputs1, inputs2);
     // end-snippet
   }
   @Test
@@ -58,7 +58,7 @@ public class CombinationTest
     // begin-snippet: YouCanVerifyCombinationsOf2
     String[] strings = {"hello", "world"};
     Integer[] numbers = {1, 2, 3};
-    CombinationApprovals.verifyAllCombinations((s, i) -> String.format("(%s,%s)", s, i), strings, numbers);
+    approvals.verifyAllCombinations((s, i) -> String.format("(%s,%s)", s, i), strings, numbers);
     // end-snippet
   }
 }
